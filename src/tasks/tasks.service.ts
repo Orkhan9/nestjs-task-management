058b8622001276d8e2c34107c663b481fa38aba6
Orkhan9 <orkhanmm@code.edu.az>
2021-07-14 23:16:29 +0400

    authorization getting tasks for user

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,34 +14,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
-    return this.taskRepository.getTasks(filterDto);
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto, user);
   }
-
-  //
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //
-  //   let tasks = this.getAllTasks();
-  //
-  //   // do something with status
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //
-  //   if (search) {
-  //     tasks = tasks.filter((task) => {
-  //       if (task.title.includes(search) || task.description.includes(search)) {
-  //         return true;
-  //       }
-  //
-  //       return false;
-  //     });
-  //   }
-  //
-  //   return tasks;
-  // }
-  //
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
